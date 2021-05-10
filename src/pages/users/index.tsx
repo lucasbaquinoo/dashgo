6620fb1c6acfc7c5bfab151edc5d2a904399659b
lucasbaquinoo <lucasbaquino@icloud.com>
2021-05-10 19:33:50 -0300

    finish to adapt to the responsive

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
 
@@ -31,17 +36,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th w="8"></Th>
+                { isWideVersion && <Th>Data de cadastro</Th> }
+                { isWideVersion && <Th w="8"></Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -50,8 +55,8 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">diego.schell.f@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
-                <Td>
+                { isWideVersion && <Td>04 de Abril, 2021</Td> }
+                { isWideVersion && <Td>
                   <Button 
                     as="a"
                     size="sm"
@@ -61,7 +66,7 @@ export default function UserList() {
                   >
                     Editar
                   </Button>
-                </Td>
+                </Td>}
               </Tr>
             </Tbody>
           </Table>
